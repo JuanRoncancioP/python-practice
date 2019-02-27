d9ed6c891f1d6eb2732f8ef4d8f43d495749594f
JuanRoncancioP <juanpabloroncancio@hotmail.es>
2019-02-27 14:11:42 -0500

    Change in the path of the txt file

--- directorio/directorio.py
+++ directorio/directorio.py
@@ -1,73 +1,7 @@
 import re
 import os
 import json
-
-
-#import Utilidades
-#import Menu
-#import Contactos
-
-
-class Utilidades:
-
-    def validaTelefono(self, telefono):
-        #Validates a telephon number string
-        patron = r"[^0-9] -"
-        return(not re.search(patron, telefono))
-
-    def validaCorreo(self, correo):
-        #Validates a mail string
-        patron = r"([\w\.\-]+)@([\w\.-]+)\.([\w\.-]+)"
-        return(re.search(patron, correo))
-
-    def confirmacion(self, label, opciones):
-        '''Shows a "Label to" the user and reads an input comparing it
-        to the "options". '''
-        op = opciones[0].lower()
-        opciones = [x[0].upper() for x in opciones]
-        while op[0] not in opciones:
-            op = input(label + ' (' + '/'.join(opciones) + '): ')
-            op = [x[0].upper() for x in op]
-        return(op)
-
-    def capturaDatos(self, label, contenedor, validacion):
-        '''Shows a message to the user,
-        validate the input and returns it into the container'''
-        respuesta = ['S']
-        print('Digite su ' + label)
-        while respuesta[0] == 'S':
-            contenedor.append(input(label + ': '))
-            if not validacion(contenedor[len(contenedor) - 1]):
-                contenedor.pop()
-                print(label + ' no valido')
-                print('pos val ', contenedor)
-            if len(contenedor):
-                respuesta = self.confirmacion('Desea ingresar otro ' +
-                label, ['S', 'N'])
-        return(contenedor)
-
-
-class Menu:
-
-    def __init__(self, opciones):
-            self.opciones = opciones
-            self.salir = max(opciones)
-            #self.objeto = objeto
-            self.listaOpciones = list(x for x in self.opciones)
-            self.opmin = str(int(min(self.listaOpciones)) - 1)
-
-    def imprimirMenu(self, titulo='GUIA TELEFONICA\n'):
-        print(titulo)
-        print('\n'.join(self.opciones[x] for x in sorted(self.opciones)))
-
-    def leerOpcion(self):
-        op = self.opmin
-        while op not in self.listaOpciones:
-            op = input('Digite su opcion: ')
-            if(op not in (self.listaOpciones)):
-                print('\tLos números del menú... Hijo!!!')
-        return(op)
-
+from src.utils import  Utilidades, Menu
 
 class Contactos:
 
@@ -75,18 +9,18 @@ class Contactos:
         self.util = Utilidades()
 
     def leerDir(self):
-        statinfo = os.stat('directorio.txt')
+        statinfo = os.stat("directorio\directorio.txt")
         if not statinfo:
             self.datos = {}
             self.llave = '1'
         else:
-            with open("directorio.txt", "r") as File:
+            with open("directorio\directorio.txt", "r") as File:
                 if bool(File):
                     self.datos = json.loads(File.read())
                     self.llave = str(int(max(self.datos)) + 1)
                     #print(self.datos, type(self.datos))
                 else:
-                    print('El directorio no pudo ser abierto')
+                    print("El directorio no pudo ser abierto")
 
     def crearContacto(self):
         nombre = input('Nombre del contacto: ')
@@ -97,7 +31,7 @@ class Contactos:
         self.llave = str(int(self.llave) + 1)
 
     def guardaDir(self):
-        with open("directorio.txt", "w") as File:
+        with open("directorio\directorio.txt", "w") as File:
             if bool(File):
                 File.write(json.dumps(self.datos))
             else:
@@ -148,10 +82,12 @@ class Contactos:
 
 
 
+def main():
+    contactos = Contactos()
+    contactos.leerDir()
+    contactos.correMenuP()
 
-contactos = Contactos()
-contactos.leerDir()
-contactos.correMenuP()
-
+if __name__ == "__main__":
+    main()
 
 
